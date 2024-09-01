869fe9edae738da4a23f502d25f0a23fa92f180b
NightProg <tonio.barbier@gmail.com>
2024-09-01 15:49:04 +0200

    Remove useless file

--- bin/main/Location.kt
+++
@@ -1,7 +0,0 @@
-
-
-data class Location(val range: IntRange, val lexeme: String, val source: Source) {
-    override fun toString(): String {
-        return lexeme
-    }
-}
--- bin/main/Regex.kt
+++
@@ -1,7 +0,0 @@
-import java.util.regex.Pattern
-
-
-fun<T> Parser<T>.regex(regex: String, f: Parser<T>.() -> T) {
-    val pattern = Pattern.compile(regex);
-
-}
